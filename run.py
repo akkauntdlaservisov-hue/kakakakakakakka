d4d440e72f8ff24032c64a9b8a12abdfeaa57401
akkauntdlaservisov-hue <akkauntdlaservisov@gmail.com>
2026-05-15 11:49:30 +0500

    Update run.py

--- run.py
+++ run.py
@@ -2,9 +2,11 @@ import subprocess
 import sys
 
 # Запускаем оба файла одновременно
-p1 = subprocess.Popen([sys.executable, "po2.py"])
-p2 = subprocess.Popen([sys.executable, "po1.py"])
+p1 = subprocess.Popen([sys.executable, "po1.py"])
+p2 = subprocess.Popen([sys.executable, "po2.py"])
+p3 = subprocess.Popen([sys.executable, "po3.py"])
 
 # Ждем завершения (они будут работать бесконечно)
 p1.wait()
 p2.wait()
+p3.wait()
